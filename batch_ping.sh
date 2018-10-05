17281c8a69db10def0e8ba31f07fb75dc04cc508
tao4free <takizawa_tao@live.com>
2018-10-05 21:59:58 +0900

    test

---
+++ batch_ping.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+for ((a=100; a <= 179 ; a++))
+do
+   #echo "Welcome $a times."
+   server=get$a
+   #sta=$(ping -c 1 "$server" 2>/dev/null)
+   #sta=`ping -c 1 $server ; echo $?`
+   #echo $server $sta > pingtest
+   output=$(ping -c 1 "$server" 2>/dev/null)
+   if [ $? -eq 0 ]; then
+     echo "$server is up" >> pingtest ; 
+     #ip=$(printf '%s' "$output" | gawk -F'[()]' '/PING/{print $2}')  
+     #echo "$ip is up";
+   else
+     echo "$server is down" >> pingtest;
+   fi
+done
+
+#for i in $(seq 100 170); do echo $i; done
+#hh={100..170}
+#for x in $hh;do
+#echo $x
+#done
+
