aa3e0fde53b5bbe2a605acc1077e3643e0ef6a42
tao4free <takizawa_tao@live.com>
2018-10-11 16:13:58 +0900

    test

--- batch_ping.sh
+++ batch_ping.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-for ((a=100; a <= 179 ; a++))
+st=210
+ed=219
+ipfile=machine.Linux.get$st$ed
+rm -rf $ipfile
+for ((a=$st; a <= $ed ; a++))
 do
    #echo "Welcome $a times."
    server=get$a
    #sta=$(ping -c 1 "$server" 2>/dev/null)
    #sta=`ping -c 1 $server ; echo $?`
    #echo $server $sta > pingtest
+   ip=` getent hosts "$server" | awk '{print $1}'`
    output=$(ping -c 1 "$server" 2>/dev/null)
    if [ $? -eq 0 ]; then
      echo "$server is up" >> pingtest ; 
+     echo $ip >> $ipfile;
      #ip=$(printf '%s' "$output" | gawk -F'[()]' '/PING/{print $2}')  
      #echo "$ip is up";
    else
