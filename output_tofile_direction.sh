b471e43de76dffe89e34ba1c9d623e4e907353cd
tao4free <takizawa_tao@live.com>
2018-10-11 16:51:17 +0900

    test

--- output_tofile_direction.sh
+++ output_tofile_direction.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+echo "#only output message to file if error"
 python3 ttt  2>xxx
-echo "1>yyy" 1>yyy
-python3 ttt 2>&1
+echo
 
+echo "#only output message to file if success" 
+echo "1>yyy" 1>yyy
+python3 xxx 1>>yyy
+echo
+
+rm xxx yyy
+
+echo "#output all message to file"
+python3 ttt 2>&1
+echo
+
+echo "#check whether last commad is successful"
 python3 xxx 2>\dev\null; echo $?
+echo
+
+echo "#check > and >>"
+for i in {1..5}; do
+ echo {1..5} > jjj
+ echo $i >> kkk
+done
