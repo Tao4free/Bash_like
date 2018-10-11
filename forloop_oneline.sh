c95bd5b19f8a3351a274d6bc18997daac22b2ede
tao4free <takizawa_tao@live.com>
2018-10-11 10:22:14 +0900

    test

--- forloop_oneline.sh
+++ forloop_oneline.sh
@@ -1,1 +1,9 @@
 for i in *.tif; do echo "${i%.*}".xyz; done
+
+
+
+for i in ./old/*.xyz; do echo $(basename -- "$i"); done
+
+
+
+for i in ./old/*.xyz; do grep -v " -9999" $i > $(basename -- "$i"); done
