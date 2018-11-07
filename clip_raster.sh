fbd18bdab8673924b72c36438da78cf38f6f009b
tao4free <takizawa_tao@live.com>
2018-11-07 17:46:33 +0900

    test

---
+++ clip_raster.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+files=`ls *.tif`
+
+key='bf3m.shp'
+tarf='../shp/'
+cutf=`ls "$tarf"*$key`
+
+#for cf in $cutf
+#do
+#  #echo $cf
+#  ver=`echo   $cf | cut -d'_' -f1 | cut -d'/' -f3`
+#  echo $ver
+#done
+
+for f in $files
+do
+  #echo $f
+  case=`echo   $f | cut -d'_' -f1`
+  #echo $case
+
+  for cf in $cutf
+  do
+    #echo $cf
+    ver=`echo   $cf | cut -d'_' -f1 | cut -d'/' -f3`
+    #echo $ver
+   
+    #if [ ${case} = ${ver} ]; then
+    #   echo "equal"
+    #fi
+
+    if [ ${case} = ${ver} ]; then
+    echo $f
+    echo $cf
+    gdalwarp -cutline $cf -crop_to_cutline -dstalpha $f clip_$f
+    fi
+
+  done  
+
+done
+
