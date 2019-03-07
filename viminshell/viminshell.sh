d51d98c018932b772325395119d5f50a5e63b703
tao4free <takizawa_tao@live.com>
2019-03-07 10:39:41 +0900

    test

--- viminshell/viminshell.sh
+++ viminshell/viminshell.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
+# reference : https://stackoverflow.com/questions/15077585/open-vim-and-run-script-upon-opening 
 
-vim sample.txt << E
-  ^[
-  k
-  k
-  k
-  dd
-  :wq
-E
+#vim sample.txt << E
+#  "+gg"
+#  "+2j"
+#  "+dd"
+#  :wq
+#E
   #for i in {1..12}
   #do
   #  #echo $i
   #  1j
   #  58dd
   #done
+vim "+1" "+3" -c ":d" -c ":wq"  sample.txt
